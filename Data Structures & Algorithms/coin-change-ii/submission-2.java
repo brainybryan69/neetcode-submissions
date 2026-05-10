30b3e1cf677e3a7717e68e317b9f8afc93e0a9af
brainybryan69 <194650180+brainybryan69@users.noreply.github.com>
2026-05-10 13:27:42 +0800

    Add: coin-change-ii - submission-2

---
+++ Data Structures & Algorithms/coin-change-ii/submission-2.java
@@ -0,0 +1,24 @@
+class Solution {
+    int[][] dp;
+
+    public int change(int amount, int[] coins) {
+        this.dp = new int[coins.length][amount + 1];
+        for (int[] row : dp) Arrays.fill(row, -1);
+
+        Arrays.sort(coins);
+        return dfs(coins, 0, amount);
+    }
+
+    private int dfs(int[] coins, int i, int amount) {
+        if (amount == 0) return 1;
+        if (amount < 0) return 0;
+        if (dp[i][amount] != -1) return dp[i][amount];
+
+        dp[i][amount] = 0;
+
+        for (int j = i; j < coins.length; j++) {
+            dp[i][amount] += dfs(coins, j, amount - coins[j]);
+        }
+        return dp[i][amount];
+    }
+}
