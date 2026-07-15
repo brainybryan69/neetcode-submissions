2fe3db7fc4d713bec97d521bdcccc8488b77ced3
brainybryan69 <194650180+brainybryan69@users.noreply.github.com>
2026-07-15 10:11:44 +0800

    Add: jump-game-ii - submission-6

---
+++ Data Structures & Algorithms/jump-game-ii/submission-6.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int jump(int[] nums) {
+        int jumps = 0;
+        int currEnd = 0;
+        int max = 0;
+
+        for (int i = 0; i < nums.length - 1; i++) {
+            max = Math.max(max, i + nums[i]);
+            if (i == currEnd) {
+                jumps++;
+                currEnd = max;
+            }
+        }
+        return jumps;
+    }
+}
